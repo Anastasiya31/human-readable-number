c4665c0f6e7d40032fd463e3fe4268b7c6ca5818
Anastasiya31 <70840370+Anastasiya31@users.noreply.github.com>
2020-10-25 22:15:08 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable (number) {
-  
+let units = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+let dozens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+
+module.exports = function toReadable(n) {
+if (n < 20) return units[n];
+let num = n % 10;
+if (n < 100) return dozens[~~(n / 10) - 2] + (num ? " " + units[num] : "");
+if (n < 1000) return units[~~(n / 100)] + " hundred" + (n % 100 == 0 ? "" : " " + toReadable(n % 100));
+return toReadable(~~(n / 1000)) + " thousand" + (n % 1000 != 0 ? " " + toReadable(n % 1000) : "");
 }
